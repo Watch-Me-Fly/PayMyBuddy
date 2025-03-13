36e3db7c62dbec1ddabb181ec1f7ca2cf1ad772e
watch-me-fly <thi.phoned@gmail.com>
2025-03-14 00:03:22 +0100

    DAL : Transaction

---
+++ src/main/java/com/paymybuddy/paymybuddy/model/Transaction.java
@@ -0,0 +1,25 @@
+package com.paymybuddy.paymybuddy.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter @Setter
+@Entity @Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+    @Column
+    private String description;
+    @Column(nullable = false)
+    private BigDecimal amount;
+}
